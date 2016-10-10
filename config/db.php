d40adba2760951f2c4c987b447b1482ce6c2da6d
pauler <thebestfromtheeastwest@gmail.com>
2016-10-10 14:46:09 +0500

    added config/

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=cafe_z',
+    'username' => 'root',
+    'password' => 'prikol',
+    'charset' => 'utf8',
+];
---
+++ config/params.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'adminEmail' => 'thebestfromtheeastwest@gmail.com',
+];
