aacfa1f85fb2e4aa56e806102b41eedd3b15a37c
pauler <thebestfromtheeastwest@gmail.com>
2016-10-10 12:46:30 +0500

    gitignore test

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=cafe_z',
     'username' => 'root',
-    'password' => 'prikol1',
+    'password' => 'prikol',
     'charset' => 'utf8',
 ];
