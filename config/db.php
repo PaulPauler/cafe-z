e828c0cac3db4518d9abd3e8f9e32cd50933ae1e
pauler <thebestfromtheeastwest@gmail.com>
2016-10-10 12:52:03 +0500

    gitignore rm -r --cached

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=cafe_z',
-    'username' => 'root',
-    'password' => 'prikol',
-    'charset' => 'utf8',
-];
